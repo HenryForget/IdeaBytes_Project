dd1cd433e185159c94d6fe26d8c4d5ce9d132f82
Inessa Khodak <khod0025@algonquinlive.com>
2024-07-11 15:41:14 -0400

    add new funcs

--- Code/analysis_coldroom.py
+++ Code/analysis_coldroom.py
@@ -1,12 +1,17 @@
 '''Running temp analysis for cold room data'''
-import temp_analysis as temp
+from temp_analysis import TempAnalysis
+from time_series_forecasting import TsForecasting
 
-dataset = temp.TempAnalysis(datapath='/mnt/c/IdeaBytes/Git/IdeaBytes_Project/Data/Temperature_NewDataSet/ColdRoom_DataLogger.csv',
+coldroom = TempAnalysis(datapath='/mnt/c/IdeaBytes/Git/IdeaBytes_Project/Data/Temperature_NewDataSet/ColdRoom_DataLogger.csv',
                             configpath='/mnt/c/IdeaBytes/Git/IdeaBytes_Project/Code/temp.conf')
-dataset.prepare_data()
+coldroom.prepare_data()
+print(coldroom.get_stats())
 dataset.plot_general(name = 'coldroom_general')
 dataset.plot_daily(name = 'coldroom_daily')
-adf = dataset.calculate_adf()
+adf = coldroom.calculate_adf()
 print(f'Dataset stationary(ADF): {adf}')
-kpss = dataset.calculate_kpss()
+kpss = coldroom.calculate_kpss()
 print(f'Dataset stationary(KPSS): {kpss}')
+if adf==kpss==True:
+    coldroom_models = TsForecasting(dataset = coldroom.data, graph_dir=coldroom.graphs_dir)
+    coldroom_models.run_arima()
