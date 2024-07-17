652194cd9ab0bb7c3c62eb1547cada9a1459b46a
Henry Forget <henry.forget.03@gmail.com>
2024-07-17 15:35:38 -0400

    KMeans Cleaned

--- Code/KMeansClustering.py
+++ Code/KMeansClustering.py
@@ -1,18 +1,8 @@
 import analysisEmerald as analysis
 import config as config
-import math
-
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-import numpy as np
-from sklearn.cluster import KMeans
 
 # Import Data
-vibedata = analysis.analysisEmerald.importer('../Data/Motor1Vibrator.csv', config.DateCol)
-
-## To be removed ##
-vibedata['index'] = range(0, len(vibedata))
-##               ##
+vibedata = analysis.analysisEmerald.importer(config.InputFile, config.DateCol)
 
 # convert all datetime values from String to datetime format and convert data values to float64 datatype
 analysis.analysisEmerald.colToDateTime(vibedata, config.DateCol, config.DateTimeFormat)
@@ -21,54 +11,7 @@ analysis.analysisEmerald.colToFlt64(vibedata, config.ValueCol)
 # Creating elbow graph
 analysis.analysisEmerald.optimise_k_means(vibedata[[config.ValueColName]], config.maxKMeans,config.ElbowFilePath)
 
-kmeans = KMeans(n_clusters=config.kMeansNumClusters)
-kmeans.fit(vibedata[['vibration (mm/s)']])
-vibedata['kmeans_3'] = kmeans.labels_
-
-anomaly = vibedata.loc[analysis.analysisEmerald.closest(vibedata['vibration (mm/s)'], 0)]['kmeans_3']
-
-pltColors = np.full(config.kMeansNumClusters, "#00FF00")
-pltColors[anomaly] = "#FF0000"
-cmap = colors.LinearSegmentedColormap.from_list("", pltColors)
-
-plt.clf()
-plt.scatter(y=vibedata['vibration (mm/s)'], x=vibedata['index'], c=vibedata['kmeans_3'], cmap=cmap)
-plt.savefig('./KMeans/KMeans' + 'OneBigFig' + '.png')
-
-# Divide the daily data
-vibedata_dividedDays = analysis.analysisEmerald.divideDays(vibedata, config.DateCol)
-
-# Derive stats from the daily data
-analysis.analysisEmerald.vibrationStats(vibedata_dividedDays, config.ValueColName, config.StatsFilePath)
-
-# Create figure and subplots
-fig,ax = plt.subplots(math.ceil(vibedata_dividedDays.__len__()/config.kMeansNumCols),config.kMeansNumCols,sharey=True)
-figX = 0
-figY = 0
-
-for n in range(vibedata_dividedDays.__len__()):
-
-     # Add scatter plot to figure
-     ax[figY][figX].scatter(y=vibedata_dividedDays[n]['vibration (mm/s)'], x=vibedata_dividedDays[n]['index'], c=vibedata_dividedDays[n]['kmeans_3'], cmap=cmap)
-     # Set title and axis labels
-     ax[figY][figX].set_title("Day " + str(n+1))
-     ax[figY][figX].set_xlabel("Data Point")
-     ax[figY][figX].set_ylabel("Vibration (mm/s)")
-
-     # Get row/col position
-     if (n + 1) % config.kMeansNumCols == 0:
-          figY += 1
-          figX = 0
-     else:
-          figX += 1
-
-# Set figure size and save
-fig.set_size_inches(config.kMeansPlotFigWidth * config.kMeansNumCols,config.kMeansPlotFigHeight * (n/config.kMeansNumCols))
-fig.tight_layout()
-fig.savefig('./KMeans/KMeans' + 'OneFig' + '.png')
-
-### TODO: Move to analysisEmerals as function
-# Print the number of anomalies per day and change below to export to a CSV
-#for n in range(vibedata.__len__()):
-#     if vibedata.loc[n]['kmeans_3'] == anomaly:
-#          print('Anomaly: ' + str(vibedata.loc[n]['Date/Time']) + '  ' + str(vibedata.loc[n]['vibration (mm/s)']))
+# KMeans analysis saved as one big figure and a sectioned figure
+analysis.analysisEmerald.KMeansBigFig(vibedata, config.BigFigFilePath) # Quick View
+print("-----------------------------------------------\n")
+analysis.analysisEmerald.KMeansSectionFig(vibedata, config.SectionFigFilePath) # Includes section stats
--- Code/config.py
+++ Code/config.py
@@ -1,35 +1,26 @@
-# TrendingVibration
-# Input: Motor#Vibrator.csv files
+# KMeansClustering
 InputFile = '../Data/Motor1Vibrator.csv'
 DateCol = 0
 DateTimeFormat = '%d-%m-%Y %H:%M'
 ValueCol = 1
+DateColName = 'Date/Time'
 ValueColName = 'vibration (mm/s)'
 StatsFilePath = '../Data/Motor1VibratorStats.csv'
-ElbowFilePath = './KMeansElbow.png'
+anomalyFilePath = '../Data/Motor1VibratorAnomaly.csv'
+ElbowFilePath = './KMeans/KMeansElbow.png'
+BigFigFilePath = './KMeans/KMeansOneBigFig.png'
+SectionFigFilePath = './KMeans/KMeansOneFig.png'
 GraphFilePath = './Vibration_Data_Set_Motor_Grouped.png'
 Decimal = 3
-std = 8.905
-avg = 21.5
-stdTreshold = 2
 maxKMeans = 10
-
-# Input: VAM_CT_Motor_1__vibration.csv
-# InputFile = '../Data/VAM_CT_Motor_1__vibration.csv'
-# DateCol = 1
-# DateTimeFormat = '%d-%m-%Y %H:%M'
-# ValueCol = 2
-# ValueColName = 'vibration mm/s'
-# StatsFilePath = ''
-# GraphFilePath = './VAM_CT_Motor_Vibration_Grouped.png'
-# Decimal = 3
-# std = 3
 
 # Kmeans graphing -----------------------------
 kMeansNumClusters = 4
 kMeansNumCols = 2
 kMeansPlotFigWidth = 10 # Width for only one sub-figure
 kMeansPlotFigHeight = 5 # Height for only one sub-figure
+goodColor = "#00FF00"
+badColor = "#FF0000"
 # ---------------------------------------------
 
 
@@ -42,3 +33,9 @@ plotRotation = 35
 # plotDaysDataframesPDF()
 plotDaysFigWidth = 200
 plotDaysFigHeight = 250
+
+
+### Not Needed ???
+std = 8.905
+avg = 21.5
+stdTreshold = 2
