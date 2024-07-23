7e12ae3ace8d1a248e796480c265872d1b3e0eca
Nico Loreto <nicoloreto23@gmail.com>
2024-07-22 22:27:30 -0400

    multi-figure now supports nx1 figures

--- Code/KMeansClustering.py
+++ Code/KMeansClustering.py
@@ -42,28 +42,43 @@ vibedata_dividedDays = analysis.analysisEmerald.divideDays(vibedata, config.Date
 analysis.analysisEmerald.vibrationStats(vibedata_dividedDays, config.ValueColName, config.StatsFilePath)
 
 # Create figure and subplots
-fig,ax = plt.subplots(math.ceil(vibedata_dividedDays.__len__()/config.kMeansNumCols),config.kMeansNumCols,sharey=True)
 figX = 0
 figY = 0
+if config.kMeansNumCols > 1:
+     fig, ax = plt.subplots(math.ceil(vibedata_dividedDays.__len__() / config.kMeansNumCols), config.kMeansNumCols,
+                            sharey=True)
+else :
+     fig, ax = plt.subplots(math.ceil(vibedata_dividedDays.__len__()), sharey=True)
 
 for n in range(vibedata_dividedDays.__len__()):
 
-     # Add scatter plot to figure
-     ax[figY][figX].scatter(y=vibedata_dividedDays[n]['vibration (mm/s)'], x=vibedata_dividedDays[n]['index'], c=vibedata_dividedDays[n]['kmeans_3'], cmap=cmap)
-     # Set title and axis labels
-     ax[figY][figX].set_title("Day " + str(n+1))
-     ax[figY][figX].set_xlabel("Data Point")
-     ax[figY][figX].set_ylabel("Vibration (mm/s)")
+     if config.kMeansNumCols > 1:
+          # Add scatter plot to figure
+          ax[figY][figX].scatter(y=vibedata_dividedDays[n]['vibration (mm/s)'], x=vibedata_dividedDays[n]['index'],
+                               c=vibedata_dividedDays[n]['kmeans_3'], cmap=cmap)
+          # Set title and axis labels
+          ax[figY][figX].set_title("Day " + str(n + 1))
+          ax[figY][figX].set_xlabel("Data Point")
+          ax[figY][figX].set_ylabel("Vibration (mm/s)")
 
-     # Get row/col position
-     if (n + 1) % config.kMeansNumCols == 0:
-          figY += 1
-          figX = 0
+          # Get row/col position
+          if (n + 1) % config.kMeansNumCols == 0:
+               figY += 1
+               figX = 0
+          else:
+               figX += 1
      else:
-          figX += 1
+          # Add scatter plot to figure
+          ax[n].scatter(y=vibedata_dividedDays[n]['vibration (mm/s)'], x=vibedata_dividedDays[n]['index'],
+                               c=vibedata_dividedDays[n]['kmeans_3'], cmap=cmap)
+          # Set title and axis labels
+          ax[n].set_title("Day " + str(n + 1))
+          ax[n].set_xlabel("Data Point")
+          ax[n].set_ylabel("Vibration (mm/s)")
 
 # Set figure size and save
-fig.set_size_inches(config.kMeansPlotFigWidth * config.kMeansNumCols,config.kMeansPlotFigHeight * (n/config.kMeansNumCols))
+fig.set_size_inches(config.kMeansPlotFigWidth * config.kMeansNumCols,
+                    config.kMeansPlotFigHeight * (vibedata_dividedDays.__len__()/config.kMeansNumCols))
 fig.tight_layout()
 fig.savefig('./KMeans/KMeans' + 'OneFig' + '.png')
 
