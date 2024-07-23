3f96832fd325480373ec46df8fe6b0dad5bade54
Henry Forget <henry.forget.03@gmail.com>
2024-07-22 22:20:40 -0400

    Merge branch 'IK'

---
+++ Code/analysis_coldroom.py
@@ -0,0 +1,19 @@
+'''Running temp analysis for cold room data'''
+from temp_analysis import TempAnalysis
+from time_series_forecasting import TsForecasting
+
+coldroom = TempAnalysis(datapath='/mnt/c/IdeaBytes/Git/IdeaBytes_Project/Data/Temperature_NewDataSet/ColdRoom_DataLogger.csv',
+                            configpath='/mnt/c/IdeaBytes/Git/IdeaBytes_Project/Code/temp.conf')
+coldroom.prepare_data()
+print(coldroom.get_stats())
+# coldroom.plot_general(name = 'coldroom_general')
+# coldroom.plot_daily(name = 'coldroom_daily')
+# adf = coldroom.calculate_adf()
+# print(f'Dataset stationary(ADF): {adf}')
+# kpss = coldroom.calculate_kpss()
+# print(f'Dataset stationary(KPSS): {kpss}')
+# if adf==kpss==True:
+coldroom_models = TsForecasting(dataset = coldroom.data, graph_dir=coldroom.graphs_dir)
+# coldroom_models.run_arima()
+# coldroom_models.run_sarimax()
+coldroom_models.run_prophet()
