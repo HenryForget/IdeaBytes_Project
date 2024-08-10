87eab564a3141486c5f5518bbe322c001d44e2d9
Inessa Khodak <khod0025@algonquinlive.com>
2024-08-09 21:51:30 -0400

    removed arima models

--- Temperature/Code/analysis_coldroom.py
+++ Temperature/Code/analysis_coldroom.py
@@ -2,18 +2,9 @@
 from temp_analysis import TempAnalysis
 from time_series_forecasting import TsForecasting
 
-coldroom = TempAnalysis(datapath='/mnt/c/IdeaBytes/Git/IdeaBytes_Project/Temperature/Data/Temperature_NewDataSet/Freezer_DataLogger1.csv',
+coldroom = TempAnalysis(datapath='/mnt/c/IdeaBytes/Git/IdeaBytes_Project/Temperature/Data/Temperature_NewDataSet/ColdRoom_DataLogger.csv',
                             configpath='/mnt/c/IdeaBytes/Git/IdeaBytes_Project/Temperature/Code/temp.conf')
 coldroom.prepare_data()
-print(coldroom.get_stats())
-coldroom.plot_general(name = 'coldroom_general')
-coldroom.plot_daily(name = 'coldroom_daily')
-adf = coldroom.calculate_adf()
-# print(f'Dataset stationary(ADF): {adf}')
-# kpss = coldroom.calculate_kpss()
-# print(f'Dataset stationary(KPSS): {kpss}')
-# if adf==kpss==True:
-# coldroom_models = TsForecasting(dataset = coldroom.data, graph_dir=coldroom.graphs_dir)
-# coldroom_models.run_arima()
-# coldroom_models.run_sarimax()
-# coldroom_models.run_prophet()
+coldroom_models = TsForecasting(dataset = coldroom.data, graph_dir=coldroom.graphs_dir)
+coldroom_models.run_prophet()
+coldroom_models.run_neural_prophet()
--- Temperature/Code/time_series_forecasting.py
+++ Temperature/Code/time_series_forecasting.py
@@ -25,59 +25,9 @@ class TsForecasting():
         self.size = int(len(self.data)*0.70)
         self.train_set, self.test_set = self.data[0:self.size], self.data[self.size:len(self.data)]
 
-    def get_acf(self):
-        '''Calculate ACF'''
-        result = acf(self.data[self.data.columns[0]])
-        return result
-
-    def plot_acf_func(self,  name):
-        '''Plots autocorrelation function'''
-        # statsmodels acf plot
-        plot_acf(self.data[self.data.columns[0]])
-        # pandas acf plot
-        # autocorrelation_plot(self.data)
-        plt.savefig(f'{"/".join([self.graph_dir,name])}.png')
-
-    def get_pacf(self):
-        '''Calculate PACF'''
-        result = pacf(self.data[self.data.columns[0]])
-        return result
-
-    def plot_pacf_func(self, name):
-        '''Plots autocorrelation function'''
-        plot_pacf(self.data[self.data.columns[0]], method = 'ols')
-        plt.savefig(f'{"/".join([self.graph_dir,name])}.png')
-
-    def plot_forecast(self, test, predict, name):
-        '''Plots results of temperature prediction'''
-        plt.figure(figsize = (50, 35))
-        plt.plot(test.index,test[test.columns[0]], color='grey', label= 'Test Set')
-        plt.plot(test.index,predict, color='blue', label = 'Prediction')
-        plt.savefig(f'{"/".join([self.graph_dir,name])}.png')
-
     def calculate_rmse(self, test, predict):
         '''Returns root squared mean error value'''
         return sqrt(mean_squared_error(test, predict))
-
-    def run_arima(self):
-        '''Uses ARIMA model for predictions
-         - no seasonal component
-         - no exogenous variables
-         - p=1, d=0, q=0'''
-        start = time.time()
-        # if not interpolate missing values in data preparation, take dataset as it is
-        # and use missing parameter to drop from the training
-        model = ARIMA(endog=self.train_set[self.train_set.columns[0]], order=(1,0,6))
-        model_fit = model.fit()
-        end = time.time()
-        # if missing values use missing='drop'
-        forecast_set = model_fit.predict(start=len(self.train_set), end=len(self.data)-1 )
-        print(f'ARIMA training time for {len(self.test_set)} test instances: \
-              {end-start} seconds.')
-        self.plot_forecast(self.test_set, forecast_set, 'ARIMA')
-        print(model_fit.summary())
-        print(f'Root Mean Squared Error: \
-              {self.calculate_rmse(self.test_set[self.test_set.columns[0]], forecast_set)}')
 
     def prepare_prophet(self, data):
         '''Renames columns to use Prophet model'''
@@ -112,31 +62,9 @@ class TsForecasting():
         plt.plot(test_set['ds'], test_set['y'], color='black', label = 'Test Set')
         plt.savefig(f'{"/".join([self.graph_dir,"Prophet_plot"])}.png')
 
-    def run_sarimax(self):
-        '''Uses SARIMAX model for predictions
-         - seasonal component - 1,0,1,12
-         - use exogenous variables
-         - p=1, d=0, q=2'''
-        start = time.time()
-        self.train_set.index = pd.DatetimeIndex(self.train_set.index).to_period('min')
-        model = SARIMAX(endog=self.train_set[self.train_set.columns[0]],
-                        exog=self.train_set[self.train_set.columns[1]], order=(1,0,4),
-                        seasonal_order=(1,0,5,6), missing='drop', freq='min')
-        model_fit = model.fit()
-        end = time.time()
-
-        forecast_set = model_fit.predict(start=len(self.train_set),
-                                         end=len(self.data)-1,
-                                         exog=self.test_set[self.test_set.columns[1]])
-        print(f'SARIMAX training time for {len(self.test_set)} test instances: \
-              {end-start} seconds.')
-        self.plot_forecast(self.test_set, forecast_set, 'SARIMAX')
-        print(model_fit.summary())
-        forecast_set.to_csv('./forecast.csv')
-        print(f'Root Mean Squared Error: {self.calculate_rmse(self.test_set[self.test_set.columns[0]], forecast_set)}')
-
     def run_neural_prophet(self):
         '''Uses Neural Prophet model for predictions'''
+        # Need to add the model to class variables to call rom another method
         data = self.prepare_prophet(self.data)
         # train_set = self.prepare_prophet(self.train_set)
         test_set = self.prepare_prophet(self.test_set)
@@ -161,4 +89,10 @@ class TsForecasting():
         plt.plot(test_set['ds'], test_set['y'], color='black', label = 'Test Set')
         plt.savefig(f'{"/".join([self.graph_dir,"NeuralProphet_plot"])}.png')
 
-
+def predict_temp(self, period):
+    '''Creates a prediction for the period specified'''
+    # takes the model (will use neural prophet)
+    # creates the future dataframe for the period specified in a call
+    # How to save the model so we can use it here? see pickle?
+    # returns the predicted values? how fast the temp goes down? how fast should we expect it to go down?
+ 
