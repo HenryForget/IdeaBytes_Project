cea03f3160ccc26d85b353ca0e2b6a4a0f3cd807
Inessa Khodak <khod0025@algonquinlive.com>
2024-10-11 11:14:02 -0400

    removed redundant

--- App/web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .webapp import views
-
-urlpatterns = [
-    # path('plot_general/', views.plot_general_view, name='plot_general'),
-    path('threshold/', views.device_thresholds, name='device_thresholds'),
-    # path('show/', views.show_graphs, name='show_graphs'),
-
-]
