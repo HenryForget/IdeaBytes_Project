72afd7917e00b11415d9f6e295fe8b684a41d15b
Hadiyah Khan <khan0465@algonquinlive.com>
2024-10-15 02:57:15 -0400

    note to self, only run the manage.py file in newproj

--- newproj/graphapp/urls.py
+++ newproj/graphapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from .views import device_Options
 
 urlpatterns = [
     path('plot_general/', views.plot_general_view, name='plot_general'),
     path('threshold/', views.device_thresholds, name='device_thresholds'),
     path('show/', views.show_graphs, name='show_graphs'),
-    path('DeviceGraph/', views.show_graphs, name='Device_Graph'),
+    path('devices/', device_Options, name='device_options'),
 
 ]
