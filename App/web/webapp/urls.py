b9be9a86cb40ae8069e784cb205a64da2aaf6804
Inessa Khodak <khod0025@algonquinlive.com>
2024-10-15 09:59:09 -0400

    added new endpoints

--- App/web/webapp/urls.py
+++ App/web/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import device_Options
 
 urlpatterns = [
     # path('plot_general/', views.plot_general_view, name='plot_general'),
     path('threshold/', views.device_thresholds, name='device_thresholds'),
     # path('show/', views.show_graphs, name='show_graphs'),
-
+    # path('devices/', device_Options, name='device_options'),
 ]
